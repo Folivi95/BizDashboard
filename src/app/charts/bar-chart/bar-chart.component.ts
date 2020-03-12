b6164465631746b81bd8b16b57c2301c19e07daf
joshuafolivi <joshua.folivi@gmail.com>
2020-03-12 08:08:04 +0100

    implemented bar chart

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SalesDataService } from 'src/app/services/sales-data.service';
+import * as moment from 'moment';
 
 // const SAMPLE_BARCHART_DATA: any[] = [
 //   { data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Sales'},
@@ -33,7 +34,38 @@ export class BarChartComponent implements OnInit {
   constructor(private salesDataService: SalesDataService) { }
 
   ngOnInit() {
-    this.salesDataService.getOrders(1, 100);
+    this.salesDataService.getOrders(1, 100).subscribe(
+      res => {
+        const localChartData = this.getChartData(res);
+        this.barChartLabels = localChartData.map(x => x[0]).reverse();
+        this.barChartData = [{data: localChartData.map(x => x[1]), label: 'Sales'}];
+      }
+    );
+  }
+
+  getChartData(res: any) {
+    this.orders = res['page']['data'];
+    const data = this.orders.map(o => o.total);
+
+    const formattedOrders = this.orders.reduce((r, e) => {
+      r.push([moment(e.placed).format('YY-MM-DD'), e.total]);
+      return r;
+    }, []);
+
+    const p = [];
+
+    const chartData = formattedOrders.reduce((r, e) => {
+      const key = e[0];
+      if (!p[key]) {
+        p[key] = e;
+        r.push(p[key]);
+      } else {
+        p[key][1] += e[1];
+      }
+      return r;
+    }, []);
+
+    return chartData;
   }
 
 }
